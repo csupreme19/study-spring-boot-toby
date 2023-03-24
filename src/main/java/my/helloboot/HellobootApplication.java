18252ba93ed76f12dabc4ce6dd3054c6ddf32b22
csupreme19 <csupreme19@gmail.com>
2023-03-24 20:36:41 +0900

    프론트 컨트롤러 패턴 서블릿 등록

--- src/main/java/my/helloboot/HellobootApplication.java
+++ src/main/java/my/helloboot/HellobootApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
@@ -18,13 +20,24 @@ public class HellobootApplication {
 		WebServer webServer = serverFactory.getWebServer(sc -> sc.addServlet("hello", new HttpServlet() {
 			@Override
 			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				String name = req.getParameter("name");
-				resp.setStatus(200);
-				resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-				resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-				resp.getWriter().println("Hello " + name);
+				String requestURI = req.getRequestURI();
+				String method = req.getMethod();
+				if(requestURI.equals("/hello") && method.equals(HttpMethod.GET.name())) {
+					new HelloController().hello(req, resp);
+				} else {
+					resp.setStatus(HttpStatus.NOT_FOUND.value());
+				}
 			}
-		}).addMapping("/hello"));
+		}).addMapping("/*"));
 		webServer.start();
+	}
+
+	static class HelloController {
+		void hello(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+			String name = req.getParameter("name");
+			resp.setStatus(HttpStatus.OK.value());
+			resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+			resp.getWriter().println("Hello " + name);
+		}
 	}
 }
