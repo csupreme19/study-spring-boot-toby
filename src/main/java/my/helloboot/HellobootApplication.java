333cc9c853a91f2491069931178cf399d8119b9e
csupreme19 <csupreme19@gmail.com>
2023-03-24 20:28:54 +0900

    서블릿 등록

--- src/main/java/my/helloboot/HellobootApplication.java
+++ src/main/java/my/helloboot/HellobootApplication.java
@@ -1,13 +1,30 @@
 package my.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 public class HellobootApplication {
 	public static void main(String[] args) {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(sc -> sc.addServlet("hello", new HttpServlet() {
+			@Override
+			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+				String name = req.getParameter("name");
+				resp.setStatus(200);
+				resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+				resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+				resp.getWriter().println("Hello " + name);
+			}
+		}).addMapping("/hello"));
 		webServer.start();
 	}
 }
