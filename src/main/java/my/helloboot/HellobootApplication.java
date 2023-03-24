702b8ccfb9cb7d41f12b4d6f993895d8be63db38
csupreme19 <csupreme19@gmail.com>
2023-03-24 21:02:49 +0900

    디스패처 서블릿 등록

--- src/main/java/my/helloboot/HellobootApplication.java
+++ src/main/java/my/helloboot/HellobootApplication.java
@@ -1,43 +1,23 @@
 package my.helloboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 	public static void main(String[] args) {
-		GenericApplicationContext ctx = new GenericApplicationContext();
+		GenericWebApplicationContext ctx = new GenericWebApplicationContext();
 		ctx.registerBean(HelloController.class);
 		ctx.registerBean(SimpleHelloService.class);
 		ctx.refresh();
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(sc -> sc.addServlet("hello", new HttpServlet() {
-			@Override
-			protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-				String requestURI = req.getRequestURI();
-				String method = req.getMethod();
-				if(requestURI.equals("/hello") && method.equals(HttpMethod.GET.name())) {
-					String name = req.getParameter("name");
-					HelloController helloController = ctx.getBean(HelloController.class);
-					String ret = helloController.hello(name);
-					resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println(ret);
-				} else {
-					resp.setStatus(HttpStatus.NOT_FOUND.value());
-				}
-			}
-		}).addMapping("/*"));
+		WebServer webServer = serverFactory
+				.getWebServer(sc -> sc.addServlet("dispatcherServlet",
+								new DispatcherServlet(ctx))
+						.addMapping("/*"));
 		webServer.start();
 	}
 }
