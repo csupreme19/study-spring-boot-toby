6f0f9d7747d6095088254872a9bef115458e614f
csupreme19 <csupreme19@gmail.com>
2023-03-25 01:06:35 +0900

    Bean 생명주기 메소드 - ApplicationContextAware

--- src/main/java/my/helloboot/HelloController.java
+++ src/main/java/my/helloboot/HelloController.java
@@ -1,5 +1,8 @@
 package my.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,10 @@ import java.util.Objects;
 
 @RequestMapping("/hello")
 @Controller
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
 
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -22,5 +26,11 @@ public class HelloController {
     @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+        this.applicationContext = applicationContext;
     }
 }
