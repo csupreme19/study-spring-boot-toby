3e305027c8a90fd2ea90b182bb709e8152bf2a42
csupreme19 <csupreme19@gmail.com>
2023-03-26 23:58:27 +0900

    서버 의존 테스트 코드 작성

--- src/main/java/my/helloboot/HellobootApplication.java
+++ src/main/java/my/helloboot/HellobootApplication.java
@@ -1,12 +1,10 @@
 package my.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
