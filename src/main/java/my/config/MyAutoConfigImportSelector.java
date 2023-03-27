0577a41fae320fd1569d82801375c145d2eef8b9
csupreme19 <csupreme19@gmail.com>
2023-03-27 18:39:21 +0900

    자동 구성 정보 파일 분리(META-INF/spring/fully-qualified-name/imports)

--- src/main/java/my/config/MyAutoConfigImportSelector.java
+++ src/main/java/my/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,22 @@
 package my.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "my.config.autoconfig.TomcatWebServerConfig",
-                "my.config.autoconfig.DispatcherServletConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+        return autoConfigs.toArray(String[]::new);
     }
 }
