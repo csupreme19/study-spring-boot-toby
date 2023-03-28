27cd3167be51496c819e1be7648e44a99ed1b261
csupreme19 <csupreme19@gmail.com>
2023-03-29 01:57:46 +0900

    자동 구성 대체하기 @ConditionalOnMissingBean

--- src/main/java/my/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/my/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package my.config.autoconfig;
 
 import my.config.ConditionalMyOnClass;
 import my.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
 
     @Bean
+    @ConditionalOnMissingBean
     public ServletWebServerFactory tomcatWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
--- src/main/java/my/config/autoconfig/UndertowWebServerConfig.java
+++ src/main/java/my/config/autoconfig/UndertowWebServerConfig.java
@@ -2,6 +2,7 @@ package my.config.autoconfig;
 
 import my.config.ConditionalMyOnClass;
 import my.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class UndertowWebServerConfig {
 
     @Bean
+    @ConditionalOnMissingBean
     public ServletWebServerFactory undertowWebServerFactory() {
         return new UndertowServletWebServerFactory();
     }
