aa23581a19bd60d6e8a87ffbe3d69d43355f3112
csupreme19 <csupreme19@gmail.com>
2023-03-29 23:24:28 +0900

    @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/my/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/my/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package my.config.autoconfig;
 
 import my.config.ConditionalMyOnClass;
 import my.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,19 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+    @Value("${contextPath:}")
+    String contextPath;
+
+    @Value("${port}")
+    int port;
+
     @Bean
     @ConditionalOnMissingBean
     public ServletWebServerFactory tomcatWebServerFactory() {
         TomcatServletWebServerFactory tomcatFactory = new TomcatServletWebServerFactory();
-        tomcatFactory.setContextPath("/app");
+        tomcatFactory.setContextPath(contextPath);
+        tomcatFactory.setPort(port);
+
         return tomcatFactory;
     }
 
